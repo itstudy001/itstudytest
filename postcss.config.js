97475498360e903a285e305219157231dc9f0095
it-mnm <noble3769@gmail.com>
2023-12-01 11:03:26 +0900

    first

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      overrideBrowserslist: [
+        // your browsers list configuration here
+      ],
+      grid: true,
+      remove: false, // This line may be needed depending on your configuration
+      browsers: ['last 2 versions', '> 1%', 'Firefox ESR', 'not dead'],
+      features: {
+        'color-adjust-pref': false,
+      },
+    },
   },
 };
